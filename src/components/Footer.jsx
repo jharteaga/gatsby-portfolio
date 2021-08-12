6dba2151fa316ceac6e99ee0bd80c618b9e5fb18
Jose Arteaga <jarteaga00@mylangara.ca>
2021-08-11 21:03:54 -0700

    Add footer component and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,35 @@
 import * as React from 'react';
+import { useStaticQuery, graphql } from 'gatsby';
 
 const Footer = () => {
-  return <h2>Footer</h2>;
+  const data = useStaticQuery(graphql`
+    query getContactPost {
+      allWpHomePosts(filter: { title: { eq: "Contact" } }) {
+        nodes {
+          homePosts {
+            description
+            tagline
+            title
+          }
+        }
+      }
+    }
+  `);
+
+  const contactPost = data.allWpHomePosts.nodes[0].homePosts;
+
+  return (
+    <footer className="site-footer" id="site-footer">
+      <div className="contact">
+        <h2 className="contact__title">{contactPost?.title}</h2>
+        <p className="contact__tagline">{contactPost?.tagline}</p>
+        <div className="contact__cta">
+          <button>{contactPost?.description}</button>
+        </div>
+      </div>
+      <p className="attribution">&copy; 2021 Copyright - Jose Arteaga</p>
+    </footer>
+  );
 };
 
 export default Footer;
